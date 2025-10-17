b26006637c5a08eb3582f87cb407171f3f61ee9a
Sanjay Joshi <sanjayjoshisj022@gmail.com>
2025-10-17 11:13:46 +0530

    Add files via upload

---
+++ color_picker.js
@@ -0,0 +1,19 @@
+function buttonColorChange1(){
+   document.getElementById("bgContainer").style.backgroundColor = "#e0e0e0";
+   document.getElementById("backgroundStatus").textContent = "#e0e0e0";
+}
+
+function buttonColorChange2(){
+    document.getElementById("bgContainer").style.backgroundColor = "#6fcf97";
+    document.getElementById("backgroundStatus").textContent = "#6fcf97";
+}
+
+function buttonColorChange3(){
+    document.getElementById("bgContainer").style.backgroundColor = "#56ccf2";
+    document.getElementById("backgroundStatus").textContent = "#56ccf2";
+}
+
+function buttonColorChange4(){
+    document.getElementById("bgContainer").style.backgroundColor = "#bb6bd9";
+    document.getElementById("backgroundStatus").textContent = "#bb6bd9";
+}
